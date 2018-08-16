d71892981a82ee8d5b23831a78ea4329a5b00f8e
agocsfruzsina <fruzsina@kokoontech.com>
2018-08-16 14:03:36 +0100

    typedef.hpp accidentally left out of RKWKB ns

--- typedefs.hpp
+++ typedefs.hpp
@@ -1,11 +1,13 @@
 #include <eigen3/Eigen/Dense>
 
-// complex numbers, vectors, matrices
-typedef Eigen::VectorXcd Vector;
-typedef Vector::Scalar Scalar;
-typedef Eigen::MatrixXcd Matrix;
-
-// complex-valued functions
-typedef std::function<Scalar(Vector)> Scalarfn;
-typedef std::function<Vector(Vector)> Vectorfn;
-typedef std::function<Matrix(Vector)> Matrixfn;
+namespace RKWKB{
+    // complex numbers, vectors, matrices
+    typedef Eigen::VectorXcd Vector;
+    typedef Vector::Scalar Scalar;
+    typedef Eigen::MatrixXcd Matrix;
+    
+    // complex-valued functions
+    typedef std::function<Scalar(Vector)> Scalarfn;
+    typedef std::function<Vector(Vector)> Vectorfn;
+    typedef std::function<Matrix(Vector)> Matrixfn;
+}
