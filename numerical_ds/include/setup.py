0b326ed308e8b2ab6a55cd56cae2de66403e987b
agocsfruzsina <fruzsina@kokoontech.com>
2019-05-04 00:20:47 +0100

    Wrapped basic solver from grids of t,w,g

--- numerical_ds/include/setup.py
+++ numerical_ds/include/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Extension
 import numpy.distutils.misc_util
 
 setup(
-    ext_modules=[Extension("_de_system",["_de_system.cpp"],include_dirs=['include'])],
+    ext_modules=[Extension("_solver",["_solver.cpp"],include_dirs=['include'])],
     include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
 )
 
