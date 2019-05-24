5e9e644039f60d374725dd627460597121d92136
fruzsinaagocs <fruzsina@kokoontech.com>
2019-05-24 18:39:17 +0100

    Improved setup.py with setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension, find_packages
 import os
 import numpy.distutils.misc_util
+
+def readme(short=False):
+    with open("README.rst") as f:
+        if short:
+            return f.readlines()[1].strip()
+        else:
+            return f.read()
+
+def get_version(short=False):
+    with open("README.rst") as f:
+        for line in f:
+            if ":Version:" in line:
+                ver = line.split(":")[2].strip()
+                if short:
+                    subver = ver.split(".")
+                    return "%s.%s" % tuple(subver[:2])
+                else:
+                    return ver
 
 pyoscode_module = Extension(
     name="_pyoscode",
@@ -11,15 +29,33 @@ pyoscode_module = Extension(
 setup(
     name="pyoscode",
     version="1.0",
-    description="Python interface to oscode 1.0",
-    url="",
+    description=readme(short=True),
+    long_description=readme(),
+    url="https://github.com/fruzsinaagocs/oscode",
     author="Fruzsina Agocs, Will Handley, Mike Hobson, and Anthony Lasenby",
     author_email="fa325@cam.ac.uk",
-    packages=["pyoscode"],
+    packages=find_packages(),
     install_requires=["numpy", "scipy"],
-    extras_require={"plotting:": "matplotlib"},
+    extras_require={"plotting:": "matplotlib", "docs":"sphinx"},
+    tests_require=["pytest"],
+    include_package_data=True,
+    license="oscode",
     ext_modules=[pyoscode_module],
     include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs(),
-    zip_safe=False
+    classifiers=[
+                'Intended Audience :: Developers',
+                'Intended Audience :: Science/Research',
+                'Natural Language :: English',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
+                'Programming Language :: Python :: 3.7',
+                'Topic :: Scientific/Engineering',
+                'Topic :: Scientific/Engineering :: Astronomy',
+                'Topic :: Scientific/Engineering :: Physics',
+                'Topic :: Scientific/Engineering :: Visualization',
+                'Topic :: Scientific/Engineering :: Mathematics',
+    ],
 )
 
